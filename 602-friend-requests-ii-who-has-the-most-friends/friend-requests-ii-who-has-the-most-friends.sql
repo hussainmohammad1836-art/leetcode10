bf4ca498aa73af51308ba8059c99950e82cff4ac
hussainmohammad1836-art <hussainmohammad1836@gmail.com>
2026-02-26 15:27:06 +0530

    Time: 299 ms (59.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+SELECT 
+    id, 
+    COUNT(id) AS num
+FROM (
+    SELECT requester_id AS id 
+    FROM RequestAccepted
+    
+    UNION ALL
+    
+    SELECT accepter_id AS id 
+    FROM RequestAccepted
+) AS t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
